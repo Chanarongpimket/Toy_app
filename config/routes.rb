86b2848d3e64618e2ba872d81d8ba16d65299bf2
ChanarongPimket <chanarongpimket@gmail.com>
2023-01-18 01:13:29 +0700

    microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :micropost
-  resources :users
-  root 'application#hello'
+  root 'user#index'
 end
