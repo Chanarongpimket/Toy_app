c55272a15c2c82ebaadc6ea1a814d4a9901eeef0
ChanarongPimket <chanarongpimket@gmail.com>
2023-01-18 01:46:08 +0700

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
-class User < ApplicationRecord
+class Micropost < ApplicationRecord
+    has_many :microposts
+    validates FILL_IN, presence: true # Replace FILL_IN with ther ight code.
+    validates FILL_IN, presence: true # Replace FILL_IN with the right code.
 end
