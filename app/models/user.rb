9c272edfa932c8d658603bbc058bfe8947e3e9fb
ChanarongPimket <chanarongpimket@gmail.com>
2023-01-19 19:55:15 +0700

    complete Fill_IN

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    # validates :FILL_IN, presence: true # Replace FILL_IN with theright code.
-    # validates :FILL_IN, presence: true # Replace FILL_IN with theright code.
+    validates :name , presence: true # Replace FILL_IN with theright code.
+    validates :email , presence: true # Replace FILL_IN with theright code.
 end
